c35d22d1e479543af1b56d0dafe5c92464950e1f
yaroslav <qwaswexexortinvoke@gmail.com>
2023-09-04 16:02:06 +0300

    stage 2 step 1: add dependencies, add Action enum to models.rs and implement Display for Status, add pages.rs with implementation of HomePage, EpicDetail and StoryDetail, add page_helpers.rs with get_column_string

--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,19 @@
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt::Display};
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Action {
+    NavigateToEpicDetail { epic_id: u32 },
+    NavigateToStoryDetail { epic_id: u32, story_id: u32 },
+    NavigateToPreviousPage,
+    CreateEpic,
+    UpdateEpicStatus { epic_id: u32 },
+    DeleteEpic { epic_id: u32 },
+    CreateStory { epic_id: u32 },
+    UpdateStoryStatus { story_id: u32 },
+    DeleteStory { epic_id: u32, story_id: u32 },
+    Exit,
+}
 
 #[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
 pub enum Status {
@@ -7,6 +21,19 @@ pub enum Status {
     InProgress,
     Resolved,
     Closed,
+}
+
+impl Display for Status {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let status = match self {
+            Status::Open => "OPEN",
+            Status::InProgress => "IN PROGRESS",
+            Status::Resolved => "RESOLVED",
+            Status::Closed => "CLOSED",
+        };
+
+        write!(f, "{}", status)
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
